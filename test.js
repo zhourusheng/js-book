8fd85058108602e9adb7287cb9291d066541bebf
zhourusheng <nfuzrs@126.com>
2019-10-14 17:12:45 +0800

    标准库object

--- test.js
+++ test.js
@@ -1,2 +1,13 @@
-const a = Math.sqrt.toString()
-console.log(a)
+var obj = {
+  p1: 123,
+  p2: 456
+}
+
+// Object.keys() 只返回可枚举的属性
+const res = Object.keys(obj)
+
+// Object.getOwnPropertyNames() 方法既返回
+const res2 = Object.getOwnPropertyNames(obj)
+
+console.log(res)
+console.log(res2)
